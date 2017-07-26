5ed4e00ed6d6d689104caffa33f291060f4f8d72
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2017-07-26 17:45:57 +0430

    002

--- 002_HttpServer/Main.go
+++ 002_HttpServer/Main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
-
-	mux :=http.NewServeMux()
-	mux.HandleFunc("/",sayHello)
-	http.ListenAndServe(":8000", mux)
+	http.HandleFunc("/",sayHello)
+	http.HandleFunc("/index", serverFile)
+	http.ListenAndServe(":8000", nil)
 }
 
 func sayHello(writer http.ResponseWriter, request *http.Request)  {
 	io.WriteString(writer,"Hello World")
+}
+
+func serverFile(responseWriter http.ResponseWriter, request *http.Request) {
+	http.ServeFile(responseWriter,request,"index.html")
 }
